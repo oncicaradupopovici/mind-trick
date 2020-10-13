2ef73293196c05b82f384b4763e41632dcfc5469
oncicaradupopovici <oncicaradupopovici@gmail.com>
2020-10-13 12:46:34 +0300

    the algebra of a mind trick

---
+++ MindTrick/Algebra.fs
@@ -0,0 +1,60 @@
+﻿module Algebra
+
+open System
+open NBB.Core.Effects
+open NBB.Core.Effects.FSharp
+
+type MindTrick<'r, 'a> = Effect<'r*'a>
+
+[<AutoOpen>]
+module MindTrick = 
+    let map (f:'a->'b) (mt:MindTrick<'r,'a>) = 
+        effect {
+            let! (r,a) = mt
+            return (r,f a)
+        }
+
+    let apply (f: MindTrick<'r, 'a->'b>) (mt:MindTrick<'r,'a>) = 
+        effect {
+            let! (_,f) = f
+            let! (r,a) = mt
+            return (r, f a)
+        }
+
+    let bind (f: 'a->MindTrick<'r,'b>) (mt: MindTrick<'r,'a>) =
+        effect {
+            let! (r,a) = mt
+            return! f a
+        }
+
+    let return' x = effect { return (x,x) }
+
+    let initialValue (mt:MindTrick<'r,'a>) = 
+        effect {
+            let! (r,_) = mt
+            return (r,r)
+        }
+
+    let liftEff (eff:Effect<'a>) = Effect.map (fun a -> (a,a)) eff
+    let lift2 f = map f >> apply
+
+    type MindTrickBuilder() =
+        member _.Bind(eff, func) = bind func eff
+        member _.Return(value) = return' value
+        member _.ReturnFrom(value) = value
+        member _.Zero() = return' ()
+        member _.Delay(f: unit->MindTrick<'r,'a>) = f |> Effect.from |> Effect.flatten
+
+    let mindTrick = MindTrickBuilder()
+
+    let (<!>) = map
+    let (>>=) eff func = bind func eff
+
+
+
+
+
+
+
+
+
---
+++ MindTrick/Program.fs
@@ -0,0 +1,34 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open NBB.Core.Effects.FSharp
+open Algebra
+
+
+let thinkOfANumber = mindTrick {
+    printfn "Think of a number"
+    return Console.ReadLine() |> int 
+}
+
+let doubleThatNumber mt = (*)2 <!> mt
+let addEight mt = (+)8 <!> mt
+let minusHalfThatNumber mt = (fun x -> x/2) <!> mt
+let minusTheNumberYouStartedWith mt = effect {
+    let! (initial, current) = mt
+    return (current, current-initial)
+}
+
+let program = thinkOfANumber |> doubleThatNumber |> addEight |> minusHalfThatNumber |> minusTheNumberYouStartedWith
+
+let p x = (x*2 + 8) /2 - x
+
+[<EntryPoint>]
+let main argv =
+    
+    let interpreter = Interpreter.createInterpreter()
+    let (_,x) = 
+        program
+        |> Effect.interpret interpreter
+        |> Async.RunSynchronously
+    printfn "The answer is %i" x
+    0 // return an integer exit code
